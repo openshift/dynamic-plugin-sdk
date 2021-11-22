610420024b0ebb2a95dca7c68aed2d53a25d2619
Andrew Ballantyne <aballant@redhat.com>
2021-11-22 16:20:09 -0500

    Add a linter to stop the 'any' type being used

--- packages/eslint-plugin-internal/rule-overrides/typescript-overrides.js
+++ packages/eslint-plugin-internal/rule-overrides/typescript-overrides.js
@@ -6,4 +6,7 @@ module.exports = {
   // Disallow variable declarations from shadowing variables declared in the outer scope
   'no-shadow': 'off',
   '@typescript-eslint/no-shadow': 'error',
+
+  // We intentionally do not want type ambiguity in the SDK
+  '@typescript-eslint/no-explicit-any': 'error',
 };
