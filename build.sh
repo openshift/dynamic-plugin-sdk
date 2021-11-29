b5446eab2416532759f2e0909e12055e618b6ffa
Vojtech Szocs <vszocs@redhat.com>
2021-11-29 18:52:40 +0100

    Update test.sh script

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -exuo pipefail
-
-yarn install
-yarn build
--- test.sh
+++ test.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 set -exuo pipefail
 
-# TODO remove this once CI runs build script prior to test script
-./build.sh
+print_error() { printf "%s\n" "$*" >&2; }
 
+# Install dependencies
+yarn install
+
+# Check for outdated yarn.lock file
+if [[ -n "$(git status --porcelain -- yarn.lock)" ]]; then
+  print_error "Outdated yarn.lock file, commit changes to fix"
+  exit 1
+fi
+
+# Analyze code for potential problems
 yarn lint
+
+# Run unit tests
 yarn test
+
+# Build packages
+yarn build
+
+# Run end-to-end tests
+# TODO
