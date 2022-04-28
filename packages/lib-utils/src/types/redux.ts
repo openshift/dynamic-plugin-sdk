7954a407d316390edf721b17a5fa1f72b3367382
Vidya Nambiar <vnambiar@redhat.com>
2022-04-28 07:35:53 -0400

    resolve typescript error using different function overload

--- packages/lib-utils/src/types/redux.ts
+++ packages/lib-utils/src/types/redux.ts
@@ -2,7 +2,7 @@ import type { Map as ImmutableMap } from 'immutable';
 import type { AnyAction } from 'redux';
 import type { ThunkDispatch } from 'redux-thunk';
 
-export type K8sState = ImmutableMap<string, unknown> | undefined;
+export type K8sState = ImmutableMap<string, unknown>;
 
 export type SDKStoreState = {
   k8s: K8sState;
