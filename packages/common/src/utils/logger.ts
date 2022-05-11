96b6c192ca2aabbf314d11cdf7ccf8ab7560878c
Vidya Nambiar <vnambiar@redhat.com>
2022-05-11 11:00:15 -0400

    add react-test-renderer
    removed unused test file
    
    rebase related updates
    
    rebase related updates

--- packages/common/src/utils/logger.ts
+++ packages/common/src/utils/logger.ts
@@ -8,14 +8,14 @@ type LogFunction = (message?: any, ...optionalParams: any[]) => void;
  */
 type Logger = Record<'info' | 'warn' | 'error', LogFunction>;
 
-const isProdEnv = () => process.env.NODE_ENV === 'production';
+const isProdEnv = process.env.NODE_ENV === 'production';
 
 /**
  * {@link Logger} implementation that uses the {@link console} API.
  */
 export const consoleLogger: Logger = {
   /* eslint-disable no-console */
-  info: (...props) => (isProdEnv() ? _.noop : console.info(...props)),
+  info: isProdEnv ? _.noop : console.info,
   warn: console.warn,
   error: console.error,
   /* eslint-enable no-console */
