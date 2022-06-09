fede3cd2ee1f500f28bbf044bde8b7629947969a
Vidya Nambiar <vnambiar@redhat.com>
2022-06-09 07:15:10 -0400

    fix query params and encode label selector correctly

--- packages/lib-utils/src/app/redux/actions/k8s.ts
+++ packages/lib-utils/src/app/redux/actions/k8s.ts
@@ -5,9 +5,9 @@ import type { ActionType as Action } from 'typesafe-actions';
 import { action } from 'typesafe-actions';
 import type { Query } from '../../../k8s/hooks/k8s-watch-types';
 import { k8sListResource, k8sGetResource } from '../../../k8s/k8s-resource';
-import { k8sWatch } from '../../../k8s/k8s-utils';
+import { k8sWatch, selectorToString } from '../../../k8s/k8s-utils';
 import type { DiscoveryResources } from '../../../types/api-discovery';
-import type { K8sModelCommon, K8sResourceCommon, FilterValue } from '../../../types/k8s';
+import type { K8sModelCommon, K8sResourceCommon, FilterValue, Selector } from '../../../types/k8s';
 import type { ThunkDispatchFunction } from '../../../types/redux';
 import type { MessageDataType } from '../../../web-socket/types';
 import type { WebSocketFactory } from '../../../web-socket/WebSocketFactory';
@@ -120,11 +120,23 @@ export const watchK8sList =
           }
         : {};
 
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { ns, ...queryParameters } = queryWithCluster; // Omit ns from query parameters
+
+      const { labelSelector } = queryParameters;
+      if (labelSelector) {
+        const encodedSelector = selectorToString(<Selector>labelSelector);
+        if (encodedSelector) {
+          queryParameters.labelSelector = encodedSelector;
+        }
+      }
+
       const response = await k8sListResource({
         model: k8skind,
         queryOptions: {
           ...queryWithCluster,
           queryParams: {
+            ...queryParameters,
             limit: `${PAGINATION_LIMIT}`,
             ...(continueToken ? { continue: continueToken } : {}),
           },
