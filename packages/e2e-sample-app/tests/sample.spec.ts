47a4120af1faec345de8286e9862e640d1c4b6cd
Vidya Nambiar <vnambiar@redhat.com>
2022-06-13 12:28:41 -0400

    updated based on comments

--- packages/e2e-sample-app/tests/sample.spec.ts
+++ packages/e2e-sample-app/tests/sample.spec.ts
@@ -1,10 +1,6 @@
-// <reference types="cypress" />
-
 describe('Sample app', () => {
   it('displays a button to load plugins', () => {
     cy.visit('/app-minimal.html');
-    cy.get('div.pf-c-toolbar__item > button.pf-c-button')
-      .first()
-      .should('have.text', 'Load plugin');
+    cy.get('[data-test-id="load-plugin-button"]').should('have.text', 'Load plugin');
   });
 });
