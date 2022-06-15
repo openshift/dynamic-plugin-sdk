f8e96aa0414e9273665ad64074d5b233dc91e1d4
Vidya Nambiar <vnambiar@redhat.com>
2022-06-15 15:26:38 -0400

    E2E script for CI

---
+++ test-prow-e2e.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+ARTIFACT_DIR=${ARTIFACT_DIR:=/tmp/artifacts}
+SCREENSHOTS_DIR=packages/e2e-sample-app/screenshots
+
+function copyArtifacts {
+  if [ -d "$ARTIFACT_DIR" ] && [ -d "$SCREENSHOTS_DIR" ]; then
+    echo "Copying artifacts from $(pwd)..."
+    cp -r "$SCREENSHOTS_DIR" "${ARTIFACT_DIR}/e2e_test_screenshots"
+  fi
+}
+
+trap copyArtifacts EXIT
+
+yarn install
+
+# Disable color codes in Cypress since they do not render well CI test logs.
+# https://docs.cypress.io/guides/guides/continuous-integration.html#Colors
+export NO_COLOR=1
+
+# Start servers for sample app and plugin on ports 9000 and 9001 respectively, run E2E tests and shut down the servers
+yarn run test-e2e-ci
