23d2e3bdc665dfbb3d86820517d39c5ae078e6eb
Vidya Nambiar <vnambiar@redhat.com>
2022-06-23 08:43:32 -0400

    Fail test to verify screenshots

--- packages/e2e-sample-app/tests/sample.spec.ts
+++ packages/e2e-sample-app/tests/sample.spec.ts
@@ -1,6 +1,6 @@
 describe('Sample application', () => {
   it('displays a button to load plugins', () => {
     cy.visit('/app-minimal.html');
-    cy.get('[data-test-id="load-plugin-button"]').should('have.text', 'Load plugin');
+    cy.get('[data-test-id="load-plugin-button"]').should('have.text', 'Load pluginx');
   });
 });
