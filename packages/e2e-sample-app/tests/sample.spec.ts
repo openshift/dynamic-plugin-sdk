c3b52624e63c83538dc021f9ff1ef35a862e1649
Vidya Nambiar <vnambiar@redhat.com>
2022-06-23 08:29:50 -0400

    Test commit to trigger PR build

--- packages/e2e-sample-app/tests/sample.spec.ts
+++ packages/e2e-sample-app/tests/sample.spec.ts
@@ -1,4 +1,4 @@
-describe('Sample app', () => {
+describe('Sample application', () => {
   it('displays a button to load plugins', () => {
     cy.visit('/app-minimal.html');
     cy.get('[data-test-id="load-plugin-button"]').should('have.text', 'Load plugin');
