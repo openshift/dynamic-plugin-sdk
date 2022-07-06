1258a745c790b86eccce00cc84fe0a47a17b601b
Filip Hlavac <filip.hlavac01@gmail.com>
2022-07-07 01:15:21 +0200

    Remove react-virtualized

--- packages/lib-utils/src/components/table-view/TableView.tsx
+++ packages/lib-utils/src/components/table-view/TableView.tsx
@@ -1,169 +1,170 @@
-import {
-  SearchInput,
-  Select,
-  SelectOption,
-  SelectVariant,
-  Toolbar,
-  ToolbarContent,
-  ToolbarItem,
-  ToolbarItemVariant,
-} from '@patternfly/react-core';
-import { FilterIcon } from '@patternfly/react-icons';
-import { omit } from 'lodash';
-import * as React from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import { parseFiltersFromURL, setFiltersToURL } from '../../utils/url-sync';
-import type { VirtualizedTableProps } from '../table/VirtualizedTable';
-import VirtualizedTable from '../table/VirtualizedTable';
-import FilterChips from './FilterChips';
-import './table-view.css';
+/* eslint-disable react/jsx-filename-extension */
+// import {
+//   SearchInput,
+//   Select,
+//   SelectOption,
+//   SelectVariant,
+//   Toolbar,
+//   ToolbarContent,
+//   ToolbarItem,
+//   ToolbarItemVariant,
+// } from '@patternfly/react-core';
+// import { FilterIcon } from '@patternfly/react-icons';
+// import { omit } from 'lodash';
+// import * as React from 'react';
+// import { useHistory, useLocation } from 'react-router-dom';
+// import { parseFiltersFromURL, setFiltersToURL } from '../../utils/url-sync';
+// import type { VirtualizedTableProps } from '../table/VirtualizedTable';
+// import VirtualizedTable from '../table/VirtualizedTable';
+// import FilterChips from './FilterChips';
+// import './table-view.css';
 
-export type FilterItem = {
-  /** Label of a parameter used for filtering. */
-  label: string;
-  /** Column name for given filtering parameter. */
-  id: string;
-};
+// export type FilterItem = {
+//   /** Label of a parameter used for filtering. */
+//   label: string;
+//   /** Column name for given filtering parameter. */
+//   id: string;
+// };
 
-export type TableViewProps<D> = VirtualizedTableProps<D> & {
-  /** Optional custom onFilter callback. */
-  onFilter?: (filterValues: Record<string, string[]>, activeFilter?: FilterItem) => D[];
-  /** Optional array of filterBy options. */
-  filters?: FilterItem[];
-};
+// export type TableViewProps<D> = VirtualizedTableProps<D> & {
+//   /** Optional custom onFilter callback. */
+//   onFilter?: (filterValues: Record<string, string[]>, activeFilter?: FilterItem) => D[];
+//   /** Optional array of filterBy options. */
+//   filters?: FilterItem[];
+// };
 
-const TableView: React.FC<TableViewProps<Record<string, unknown>>> = ({
-  columns,
-  data,
-  filters = [],
-  isRowSelected,
-  onSelect,
-  onFilter,
-  loadError,
-  loaded,
-  Row,
-  CustomEmptyState,
-  emptyStateDescription,
-  loadErrorDefaultText,
-  CustomNoDataEmptyState,
-  'aria-label': ariaLabel,
-}) => {
-  const history = useHistory();
-  const location = useLocation();
-  const [activeFilter, setActiveFilter] = React.useState<FilterItem | undefined>(filters?.[0]);
-  const [filteredData, setFilteredData] = React.useState(data);
-  const [isFilterSelectExpanded, setFilterSelectExpanded] = React.useState(false);
-  const filterValues = React.useRef<Record<string, string[]>>({});
+// const TableView: React.FC<TableViewProps<Record<string, unknown>>> = ({
+//   columns,
+//   data,
+//   filters = [],
+//   isRowSelected,
+//   onSelect,
+//   onFilter,
+//   loadError,
+//   loaded,
+//   Row,
+//   CustomEmptyState,
+//   emptyStateDescription,
+//   loadErrorDefaultText,
+//   CustomNoDataEmptyState,
+//   'aria-label': ariaLabel,
+// }) => {
+//   const history = useHistory();
+//   const location = useLocation();
+//   const [activeFilter, setActiveFilter] = React.useState<FilterItem | undefined>(filters?.[0]);
+//   const [filteredData, setFilteredData] = React.useState(data);
+//   const [isFilterSelectExpanded, setFilterSelectExpanded] = React.useState(false);
+//   const filterValues = React.useRef<Record<string, string[]>>({});
 
-  React.useEffect(() => {
-    filterValues.current = parseFiltersFromURL(
-      new URLSearchParams(location.search),
-      filters.map((filter) => filter.id),
-    );
-    if (filters) {
-      setFilteredData(
-        onFilter
-          ? onFilter(filterValues.current, activeFilter)
-          : [...data].filter((item) => {
-              let isRelevant = true;
-              Object.keys(filterValues.current).forEach((key) => {
-                if (
-                  filterValues.current[key].some(
-                    (filterValue) => !(item[key] as string)?.toLowerCase()?.includes(filterValue),
-                  )
-                ) {
-                  isRelevant = false;
-                }
-              });
-              return isRelevant;
-            }),
-      );
-    }
-  }, [location, activeFilter, data, filters, onFilter]);
+//   React.useEffect(() => {
+//     filterValues.current = parseFiltersFromURL(
+//       new URLSearchParams(location.search),
+//       filters.map((filter) => filter.id),
+//     );
+//     if (filters) {
+//       setFilteredData(
+//         onFilter
+//           ? onFilter(filterValues.current, activeFilter)
+//           : [...data].filter((item) => {
+//               let isRelevant = true;
+//               Object.keys(filterValues.current).forEach((key) => {
+//                 if (
+//                   filterValues.current[key].some(
+//                     (filterValue) => !(item[key] as string)?.toLowerCase()?.includes(filterValue),
+//                   )
+//                 ) {
+//                   isRelevant = false;
+//                 }
+//               });
+//               return isRelevant;
+//             }),
+//       );
+//     }
+//   }, [location, activeFilter, data, filters, onFilter]);
 
-  return (
-    <>
-      <Toolbar>
-        <ToolbarContent>
-          {filters ? (
-            <>
-              <ToolbarItem key="filter-select">
-                <Select
-                  toggleIcon={<FilterIcon />}
-                  variant={SelectVariant.single}
-                  onToggle={(value) => setFilterSelectExpanded(value)}
-                  onSelect={(e, selection) => {
-                    setActiveFilter(filters.find((item) => item.id === selection));
-                    setFilterSelectExpanded(false);
-                  }}
-                  placeholderText={activeFilter?.label}
-                  isOpen={isFilterSelectExpanded}
-                >
-                  {filters.map((option) => (
-                    <SelectOption key={option.id} value={option.id}>
-                      {option.label}
-                    </SelectOption>
-                  ))}
-                </Select>
-              </ToolbarItem>
-              <ToolbarItem variant={ToolbarItemVariant['search-filter']} key="search-filter">
-                <SearchInput
-                  className="dps-table-view__search"
-                  onChange={(value) => {
-                    if (activeFilter) {
-                      const newValues =
-                        value?.length > 0
-                          ? { ...filterValues.current, [activeFilter.id]: [value] }
-                          : omit(filterValues.current, activeFilter.id);
-                      setFiltersToURL(
-                        history,
-                        filters.map((filter) => filter.id),
-                        newValues,
-                      );
-                    }
-                  }}
-                  value={activeFilter ? filterValues.current[activeFilter.id]?.[0] : ''}
-                  placeholder={`Filter by ${activeFilter?.label}`}
-                />
-              </ToolbarItem>
-            </>
-          ) : null}
-        </ToolbarContent>
-        {Object.keys(filterValues.current)?.length > 0 && (
-          <ToolbarContent className="dps-table-view__filters">
-            <ToolbarItem>
-              <FilterChips
-                filters={filters}
-                filterValues={filterValues.current}
-                onDelete={(key) => {
-                  setFiltersToURL(
-                    history,
-                    filters.map((filter) => filter.id),
-                    key ? omit(filterValues.current, key) : {},
-                  );
-                }}
-              />
-            </ToolbarItem>
-          </ToolbarContent>
-        )}
-      </Toolbar>
-      <VirtualizedTable
-        aria-label={ariaLabel}
-        areFiltersApplied={Object.values(filterValues.current).some((value) => value?.length > 0)}
-        data={filters ? filteredData : data}
-        loaded={loaded}
-        columns={columns}
-        isRowSelected={isRowSelected}
-        onSelect={onSelect}
-        Row={Row}
-        emptyStateDescription={emptyStateDescription}
-        CustomEmptyState={CustomEmptyState}
-        loadError={loadError}
-        loadErrorDefaultText={loadErrorDefaultText}
-        CustomNoDataEmptyState={CustomNoDataEmptyState}
-      />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <Toolbar>
+//         <ToolbarContent>
+//           {filters ? (
+//             <>
+//               <ToolbarItem key="filter-select">
+//                 <Select
+//                   toggleIcon={<FilterIcon />}
+//                   variant={SelectVariant.single}
+//                   onToggle={(value) => setFilterSelectExpanded(value)}
+//                   onSelect={(e, selection) => {
+//                     setActiveFilter(filters.find((item) => item.id === selection));
+//                     setFilterSelectExpanded(false);
+//                   }}
+//                   placeholderText={activeFilter?.label}
+//                   isOpen={isFilterSelectExpanded}
+//                 >
+//                   {filters.map((option) => (
+//                     <SelectOption key={option.id} value={option.id}>
+//                       {option.label}
+//                     </SelectOption>
+//                   ))}
+//                 </Select>
+//               </ToolbarItem>
+//               <ToolbarItem variant={ToolbarItemVariant['search-filter']} key="search-filter">
+//                 <SearchInput
+//                   className="dps-table-view__search"
+//                   onChange={(value) => {
+//                     if (activeFilter) {
+//                       const newValues =
+//                         value?.length > 0
+//                           ? { ...filterValues.current, [activeFilter.id]: [value] }
+//                           : omit(filterValues.current, activeFilter.id);
+//                       setFiltersToURL(
+//                         history,
+//                         filters.map((filter) => filter.id),
+//                         newValues,
+//                       );
+//                     }
+//                   }}
+//                   value={activeFilter ? filterValues.current[activeFilter.id]?.[0] : ''}
+//                   placeholder={`Filter by ${activeFilter?.label}`}
+//                 />
+//               </ToolbarItem>
+//             </>
+//           ) : null}
+//         </ToolbarContent>
+//         {Object.keys(filterValues.current)?.length > 0 && (
+//           <ToolbarContent className="dps-table-view__filters">
+//             <ToolbarItem>
+//               <FilterChips
+//                 filters={filters}
+//                 filterValues={filterValues.current}
+//                 onDelete={(key) => {
+//                   setFiltersToURL(
+//                     history,
+//                     filters.map((filter) => filter.id),
+//                     key ? omit(filterValues.current, key) : {},
+//                   );
+//                 }}
+//               />
+//             </ToolbarItem>
+//           </ToolbarContent>
+//         )}
+//       </Toolbar>
+//       <VirtualizedTable
+//         aria-label={ariaLabel}
+//         areFiltersApplied={Object.values(filterValues.current).some((value) => value?.length > 0)}
+//         data={filters ? filteredData : data}
+//         loaded={loaded}
+//         columns={columns}
+//         isRowSelected={isRowSelected}
+//         onSelect={onSelect}
+//         Row={Row}
+//         emptyStateDescription={emptyStateDescription}
+//         CustomEmptyState={CustomEmptyState}
+//         loadError={loadError}
+//         loadErrorDefaultText={loadErrorDefaultText}
+//         CustomNoDataEmptyState={CustomNoDataEmptyState}
+//       />
+//     </>
+//   );
+// };
 
-export default TableView;
+// export default TableView;
--- packages/lib-utils/src/components/table/VirtualizedTableBody.tsx
+++ packages/lib-utils/src/components/table/VirtualizedTableBody.tsx
@@ -1,156 +1,157 @@
-import { Td } from '@patternfly/react-table';
-import type { ICell, SortByDirection, ThProps } from '@patternfly/react-table';
-import { VirtualTableBody } from '@patternfly/react-virtualized-extension';
-import type { Scroll } from '@patternfly/react-virtualized-extension/dist/js/components/Virtualized/types';
-import * as React from 'react';
-import { CellMeasurerCache, CellMeasurer } from 'react-virtualized';
-import type { MeasuredCellParent } from 'react-virtualized/dist/es/CellMeasurer';
+/* eslint-disable react/jsx-filename-extension */
+// import { Td } from '@patternfly/react-table';
+// import type { ICell, SortByDirection, ThProps } from '@patternfly/react-table';
+// import { VirtualTableBody } from '@patternfly/react-virtualized-extension';
+// import type { Scroll } from '@patternfly/react-virtualized-extension/dist/js/components/Virtualized/types';
+// import * as React from 'react';
+// import { CellMeasurerCache, CellMeasurer } from 'react-virtualized';
+// import type { MeasuredCellParent } from 'react-virtualized/dist/es/CellMeasurer';
 
-export type RowProps<D> = {
-  /** Row data object. */
-  obj: D;
-};
+// export type RowProps<D> = {
+//   /** Row data object. */
+//   obj: D;
+// };
 
-type RowMemoProps<D> = RowProps<D> & { Row: React.ComponentType<RowProps<D>> };
+// type RowMemoProps<D> = RowProps<D> & { Row: React.ComponentType<RowProps<D>> };
 
-export type TableColumn<D> = ICell & {
-  /** Column ID. */
-  id: string;
-  /** Optional sort configuration. */
-  sort?: ((data: D[], sortDirection: SortByDirection) => D[]) | ThProps['sort'] | string;
-  /** Optional visibility. */
-  visibility?: string[];
-};
+// export type TableColumn<D> = ICell & {
+//   /** Column ID. */
+//   id: string;
+//   /** Optional sort configuration. */
+//   sort?: ((data: D[], sortDirection: SortByDirection) => D[]) | ThProps['sort'] | string;
+//   /** Optional visibility. */
+//   visibility?: string[];
+// };
 
-export type TableRowProps = {
-  /** Row ID. */
-  id: string;
-  /** Row index. */
-  index: number;
-  /** Optional row style. */
-  style: object;
-  /** Row key. */
-  trKey: string;
-  /** Optional className. */
-  className?: string;
-};
+// export type TableRowProps = {
+//   /** Row ID. */
+//   id: string;
+//   /** Row index. */
+//   index: number;
+//   /** Optional row style. */
+//   style: object;
+//   /** Row key. */
+//   trKey: string;
+//   /** Optional className. */
+//   className?: string;
+// };
 
-export const TableRow: React.FC<TableRowProps> = ({ id, children, style, trKey, className }) => (
-  <tr id={id} style={style} key={trKey} className={className} role="row">
-    {children}
-  </tr>
-);
+// export const TableRow: React.FC<TableRowProps> = ({ id, children, style, trKey, className }) => (
+//   <tr id={id} style={style} key={trKey} className={className} role="row">
+//     {children}
+//   </tr>
+// );
 
-type VirtualizedTableBodyProps<D> = {
-  /** Table columns. */
-  columns: TableColumn<D>[];
-  /** Data to be rendered. */
-  data: D[];
-  /** Table body height. */
-  height: number;
-  /** isScrolling flag. */
-  isScrolling: boolean;
-  /** onChildScroll callback. */
-  onChildScroll: (params: Scroll) => void;
-  /** Row component. */
-  Row: React.ComponentType<RowProps<D>>;
-  /** Optional isSelected row callback */
-  isRowSelected?: (item: D) => boolean;
-  /** Optional onSelect row callback */
-  onSelect?: (event: React.FormEvent<HTMLInputElement>, isRowSelected: boolean, data: D[]) => void;
-  /** Scroll top number. */
-  scrollTop: number;
-  /** Table body width. */
-  width: number;
-};
+// type VirtualizedTableBodyProps<D> = {
+//   /** Table columns. */
+//   columns: TableColumn<D>[];
+//   /** Data to be rendered. */
+//   data: D[];
+//   /** Table body height. */
+//   height: number;
+//   /** isScrolling flag. */
+//   isScrolling: boolean;
+//   /** onChildScroll callback. */
+//   onChildScroll: (params: Scroll) => void;
+//   /** Row component. */
+//   Row: React.ComponentType<RowProps<D>>;
+//   /** Optional isSelected row callback */
+//   isRowSelected?: (item: D) => boolean;
+//   /** Optional onSelect row callback */
+//   onSelect?: (event: React.FormEvent<HTMLInputElement>, isRowSelected: boolean, data: D[]) => void;
+//   /** Scroll top number. */
+//   scrollTop: number;
+//   /** Table body width. */
+//   width: number;
+// };
 
-const VirtualizedTableBody = <D,>({
-  columns,
-  data,
-  height,
-  isScrolling,
-  onChildScroll,
-  Row,
-  isRowSelected,
-  onSelect,
-  scrollTop,
-  width,
-}: VirtualizedTableBodyProps<D>) => {
-  const cellMeasurementCache = new CellMeasurerCache({
-    fixedWidth: true,
-    minHeight: 44,
-    keyMapper: (rowIndex: number) =>
-      (data?.[rowIndex] as unknown as Record<string, Record<string, unknown>>)?.metadata?.uid ??
-      rowIndex,
-  });
+// const VirtualizedTableBody = <D,>({
+//   columns,
+//   data,
+//   height,
+//   isScrolling,
+//   onChildScroll,
+//   Row,
+//   isRowSelected,
+//   onSelect,
+//   scrollTop,
+//   width,
+// }: VirtualizedTableBodyProps<D>) => {
+//   const cellMeasurementCache = new CellMeasurerCache({
+//     fixedWidth: true,
+//     minHeight: 44,
+//     keyMapper: (rowIndex: number) =>
+//       (data?.[rowIndex] as unknown as Record<string, Record<string, unknown>>)?.metadata?.uid ??
+//       rowIndex,
+//   });
 
-  // eslint-disable-next-line react/prop-types -- this rule has issues with React.memo
-  const RowMemo: React.FC<RowMemoProps<D>> = React.memo(({ Row: RowComponent, obj }) => (
-    <RowComponent obj={obj} />
-  ));
+//   // eslint-disable-next-line react/prop-types -- this rule has issues with React.memo
+//   const RowMemo: React.FC<RowMemoProps<D>> = React.memo(({ Row: RowComponent, obj }) => (
+//     <RowComponent obj={obj} />
+//   ));
 
-  type RowRendererParams = {
-    index: number;
-    key: string;
-    parent: MeasuredCellParent;
-    style: object;
-    isScrolling: boolean;
-    isVisible: boolean;
-  };
+//   type RowRendererParams = {
+//     index: number;
+//     key: string;
+//     parent: MeasuredCellParent;
+//     style: object;
+//     isScrolling: boolean;
+//     isVisible: boolean;
+//   };
 
-  const rowRenderer = ({ index, isVisible, key, parent, style }: RowRendererParams) => {
-    const rowArgs: RowProps<D> = {
-      obj: data[index],
-    };
+//   const rowRenderer = ({ index, isVisible, key, parent, style }: RowRendererParams) => {
+//     const rowArgs: RowProps<D> = {
+//       obj: data[index],
+//     };
 
-    // do not render non visible elements (this excludes overscan)
-    if (!isVisible) {
-      return null;
-    }
+//     // do not render non visible elements (this excludes overscan)
+//     if (!isVisible) {
+//       return null;
+//     }
 
-    return (
-      <CellMeasurer
-        cache={cellMeasurementCache}
-        columnIndex={0}
-        key={key}
-        parent={parent}
-        rowIndex={index}
-      >
-        <TableRow id={key} index={index} trKey={key} style={style}>
-          {onSelect && (
-            <Td
-              select={{
-                rowIndex: index,
-                onSelect: (event, isSelected) => onSelect?.(event, isSelected, [rowArgs.obj]),
-                isSelected: isRowSelected?.(rowArgs.obj) || false,
-                disable: !!(rowArgs?.obj as Record<string, unknown>)?.disable,
-              }}
-            />
-          )}
-          <RowMemo Row={Row} obj={rowArgs.obj} />
-        </TableRow>
-      </CellMeasurer>
-    );
-  };
+//     return (
+//       <CellMeasurer
+//         cache={cellMeasurementCache}
+//         columnIndex={0}
+//         key={key}
+//         parent={parent}
+//         rowIndex={index}
+//       >
+//         <TableRow id={key} index={index} trKey={key} style={style}>
+//           {onSelect && (
+//             <Td
+//               select={{
+//                 rowIndex: index,
+//                 onSelect: (event, isSelected) => onSelect?.(event, isSelected, [rowArgs.obj]),
+//                 isSelected: isRowSelected?.(rowArgs.obj) || false,
+//                 disable: !!(rowArgs?.obj as Record<string, unknown>)?.disable,
+//               }}
+//             />
+//           )}
+//           <RowMemo Row={Row} obj={rowArgs.obj} />
+//         </TableRow>
+//       </CellMeasurer>
+//     );
+//   };
 
-  return (
-    <VirtualTableBody
-      autoHeight
-      columns={columns}
-      className="pf-c-table pf-m-border-rows pf-c-virtualized pf-c-window-scroller"
-      deferredMeasurementCache={cellMeasurementCache}
-      height={height || 0}
-      isScrolling={isScrolling}
-      onScroll={onChildScroll}
-      overscanRowCount={10}
-      rowCount={data.length}
-      rowHeight={cellMeasurementCache.rowHeight}
-      rowRenderer={rowRenderer}
-      rows={data}
-      scrollTop={scrollTop}
-      width={width}
-    />
-  );
-};
+//   return (
+//     <VirtualTableBody
+//       autoHeight
+//       columns={columns}
+//       className="pf-c-table pf-m-border-rows pf-c-virtualized pf-c-window-scroller"
+//       deferredMeasurementCache={cellMeasurementCache}
+//       height={height || 0}
+//       isScrolling={isScrolling}
+//       onScroll={onChildScroll}
+//       overscanRowCount={10}
+//       rowCount={data.length}
+//       rowHeight={cellMeasurementCache.rowHeight}
+//       rowRenderer={rowRenderer}
+//       rows={data}
+//       scrollTop={scrollTop}
+//       width={width}
+//     />
+//   );
+// };
 
-export default VirtualizedTableBody;
+// export default VirtualizedTableBody;
