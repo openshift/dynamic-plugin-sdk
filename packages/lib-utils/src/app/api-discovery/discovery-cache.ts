b42f645987f691e0be622306ba432e51bcacad57
Karel Hala <khala@redhat.com>
2022-07-27 12:55:54 +0200

    Use lodash's mergeWith and add two new functions to store and get resources

--- packages/lib-utils/src/app/api-discovery/discovery-cache.ts
+++ packages/lib-utils/src/app/api-discovery/discovery-cache.ts
@@ -9,42 +9,44 @@ const SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY = 'sdk/api-discovery-resourc
 const mergeByKey = (prev: K8sModelCommon[], next: K8sModelCommon[]) =>
   Object.values(_.merge(_.keyBy(prev, getReferenceForModel), _.keyBy(next, getReferenceForModel)));
 
-export const cacheResources = (resources: DiscoveryResources[]) => {
-  let allResources;
+const getLocalResources = () => {
   try {
-    allResources = [
-      ...[JSON.parse(localStorage.getItem(SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY) || '{}')],
-      ...resources,
-    ].reduce((acc, curr) => {
-      const { models, configResources, clusterOperatorConfigResources, ...rest } = curr || {};
-      return {
-        ..._.merge(acc, rest),
-        configResources: mergeByKey(acc?.configResources, configResources || []),
-        models: mergeByKey(acc?.models, models || []),
-        clusterOperatorConfigResources: mergeByKey(
-          acc?.clusterOperatorConfigResources,
-          clusterOperatorConfigResources || [],
-        ),
-      };
-    }, {});
+    return JSON.parse(localStorage.getItem(SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY) || '{}');
   } catch (e) {
-    consoleLogger.error('Error caching API resources in localStorage', e);
+    consoleLogger.error('Cannot load cached API resources', e);
     throw e;
   }
+};
+
+const setLocalResources = (resources: DiscoveryResources[]) => {
   try {
-    localStorage.setItem(
-      SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY,
-      JSON.stringify(allResources),
-    );
+    localStorage.setItem(SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY, JSON.stringify(resources));
   } catch (e) {
     consoleLogger.error('Error caching API resources in localStorage', e);
     throw e;
   }
 };
+export const cacheResources = (resources: DiscoveryResources[]) => {
+  const allResources = [...[getLocalResources()], ...resources].reduce(
+    (acc, curr) =>
+      _.mergeWith(acc, curr, (first, second) => {
+        if (Array.isArray(first) && first[0]?.constructor?.name === 'Object') {
+          return mergeByKey(first, second);
+        }
+
+        return undefined;
+      }),
+    {},
+  );
+  setLocalResources(allResources);
+};
 
 export const getCachedResources = () => {
   const resourcesJSON = localStorage.getItem(SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY);
   if (!resourcesJSON) {
+    consoleLogger.error(
+      `No API resources found in localStorage for key ${SDK_API_DISCOVERY_RESOURCES_LOCAL_STORAGE_KEY}`,
+    );
     return null;
   }
 
