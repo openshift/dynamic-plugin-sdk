ba64d2e6ed621f7f6357eb72ab0c6ea8c9653c84
Blake Holifield <bholifie@redhat.com>
2023-02-22 10:45:00 -0500

    update docstrings for workspace context and provider

--- packages/lib-utils/src/utils/WorkspaceContext.ts
+++ packages/lib-utils/src/utils/WorkspaceContext.ts
@@ -1,6 +1,16 @@
 import { createContext } from 'react';
 import type { workspaceState } from './workspaceState';
 
+/**
+ *
+ * The WorkspaceContext is a context for the `workspaceState`. It is used to access the methods
+ * and data used to implement an `activeWorkspace`.
+ *
+ * @example
+ * ``` ts
+ * const { subscribe, unsubscribe, getState, setWorkspaceContext } = useContext(WorkspaceContext);
+ * ```
+ */
 const WorkspaceContext = createContext<ReturnType<typeof workspaceState>>({
   update: () => undefined,
   setWorkspaceContext: () => undefined,
