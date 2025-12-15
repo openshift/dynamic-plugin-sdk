d44a7333407d8167b94634cc5d7ff532078581fa
logonoff <git@logonoff.co>
2025-12-15 11:47:56 -0500

    fix: remove as cast and fix todo

--- packages/sample-plugin/plugin-extensions.ts
+++ packages/sample-plugin/plugin-extensions.ts
@@ -4,25 +4,26 @@ import type {
 } from '@monorepo/sample-app/src/sample-extensions';
 import type { EncodedExtension } from '@openshift/dynamic-plugin-sdk';
 
-// TODO(vojtech): make EncodedExtension<T> type param work with A | B unions
+type ConsumedExtension =
+  | EncodedExtension<SampleAppExtensionWithText>
+  | EncodedExtension<SampleAppExtensionWithComponent>;
 
-const extensions: EncodedExtension[] = [];
-
-extensions.push({
-  type: 'sample-app.text',
-  properties: {
-    text: 'Plasma reactors online',
+const extensions: ConsumedExtension[] = [
+  {
+    type: 'sample-app.text',
+    properties: {
+      text: 'Plasma reactors online',
+    },
   },
-} as EncodedExtension<SampleAppExtensionWithText>);
-
-extensions.push({
-  type: 'sample-app.component',
-  properties: {
-    component: { $codeRef: 'testComponent' },
+  {
+    type: 'sample-app.component',
+    properties: {
+      component: { $codeRef: 'testComponent' },
+    },
+    flags: {
+      required: ['SAMPLE_FLAG'],
+    },
   },
-  flags: {
-    required: ['SAMPLE_FLAG'],
-  },
-} as EncodedExtension<SampleAppExtensionWithComponent>);
+];
 
 export default extensions;
