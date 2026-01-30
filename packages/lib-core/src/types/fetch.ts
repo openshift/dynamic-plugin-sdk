d40a6772e7b8ba08617cc823670ada34baade84f
logonoff <git@logonoff.co>
2026-01-29 19:25:36 -0500

    Small change

--- packages/lib-core/src/types/fetch.ts
+++ packages/lib-core/src/types/fetch.ts
@@ -1,4 +1,5 @@
 /**
- * Fetch a resource over HTTP and return the {@link Response} object.
+ * An implementation of {@link fetch} that fetches a resource over HTTP
+ * and returns the {@link Response} object.
  */
 export type ResourceFetch = (url: string, requestInit?: RequestInit) => Promise<Response>;
